57c63cf813a1afe3096588cf8491ed740f9915ba
Masahiro Honma <hiratara@cpan.org>
2015-10-25 17:30:04 +0900

    Split the mutex into 10 units
    Improve performance from 1908281600 ns/op to 230503340 ns/op

--- fetchmgr.go
+++ fetchmgr.go
@@ -1,6 +1,7 @@
 package fetchmgr
 
 import (
+	"hash/fnv"
 	"sync"
 	"time"
 )
@@ -21,11 +22,12 @@ func (f FuncFetcher) Fetch(k interface{}) (interface{}, error) {
 // CachedFetcher caches fetched contents. It use Fetcher internally to fetch
 // resources. It will call Fetcher's Fetch method thread-safely.
 type CachedFetcher struct {
-	fetcher Fetcher
-	mutex   sync.Mutex
-	ttl     time.Duration
-	expires time.Time
-	cache   map[interface{}]interface{}
+	fetcher   Fetcher
+	ttl       time.Duration
+	expires   time.Time
+	bucketNum uint
+	mutex     []sync.Mutex
+	cache     []map[interface{}]interface{}
 }
 
 // New creates CachedFetcher
@@ -33,13 +35,20 @@ func New(
 	fetcher Fetcher,
 	ttl time.Duration,
 ) *CachedFetcher {
-	cache := &CachedFetcher{
-		fetcher: fetcher,
-		ttl:     ttl,
-	}
-	cache.prepare()
+	n := 10
+	mutex := make([]sync.Mutex, n)
+	cache := make([]map[interface{}]interface{}, n)
 
-	return cache
+	cached := &CachedFetcher{
+		fetcher:   fetcher,
+		ttl:       ttl,
+		bucketNum: uint(n),
+		mutex:     mutex,
+		cache:     cache,
+	}
+	cached.prepare()
+
+	return cached
 }
 
 // Fetch memoizes fetcher.Fetch method.
@@ -48,12 +57,14 @@ func New(
 // If the internal Fetcher.Fetch returns err (!= nil), CachedFetcher doesn't
 // cache any results.
 func (c *CachedFetcher) Fetch(key interface{}) (interface{}, error) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	h := hash(key) % c.bucketNum
+
+	c.mutex[h].Lock()
+	defer c.mutex[h].Unlock()
 
 	c.cleanup()
 
-	cached, ok := c.cache[key]
+	cached, ok := c.cache[h][key]
 	if ok {
 		return cached, nil
 	}
@@ -63,12 +74,14 @@ func (c *CachedFetcher) Fetch(key interface{}) (interface{}, error) {
 		return val, err
 	}
 
-	c.cache[key] = val
+	c.cache[h][key] = val
 	return val, nil
 }
 
 func (c *CachedFetcher) prepare() {
-	c.cache = map[interface{}]interface{}{}
+	for i := range c.cache {
+		c.cache[i] = map[interface{}]interface{}{}
+	}
 	c.expires = time.Now().Add(c.ttl)
 }
 
@@ -78,4 +91,41 @@ func (c *CachedFetcher) cleanup() {
 	}
 
 	c.prepare()
+}
+
+func hash(k interface{}) uint {
+	switch kk := k.(type) {
+	case Hasher:
+		return kk.Hash()
+	case int:
+		kkk := KInt(kk)
+		return kkk.Hash()
+	case string:
+		return KStr(kk).Hash()
+	}
+	return 0
+}
+
+// Hasher provides a function in order to calculate its hash values
+type Hasher interface {
+	Hash() uint
+}
+
+// KInt is hashable int
+type KInt int
+
+// Hash calculates hash values
+func (k KInt) Hash() uint {
+	return uint(k)
+}
+
+// KStr is hashable string
+type KStr string
+
+// Hash calculates hash values
+func (k KStr) Hash() uint {
+	h := fnv.New64a()
+	h.Write([]byte(k))
+
+	return uint(h.Sum64())
 }
