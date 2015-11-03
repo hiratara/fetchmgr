b99e5b96f9e316522e25782018ee1c47d86a9b8b
Masahiro Honma <hiratara@cpan.org>
2015-11-03 13:24:46 +0900

    Implement the delete loop
    Though it has stupid polling loop :/

--- cacher.go
+++ cacher.go
@@ -1,6 +1,7 @@
 package fetchmgr
 
 import (
+	"container/heap"
 	"sync"
 	"time"
 )
@@ -8,10 +9,12 @@ import (
 // CachedFetcher caches fetched contents. It use Fetcher internally to fetch
 // resources. It will call Fetcher's Fetch method thread-safely.
 type CachedFetcher struct {
-	fetcher Fetcher
-	ttl     time.Duration
-	mutex   sync.Mutex
-	cache   map[interface{}]entry
+	fetcher  Fetcher
+	ttl      time.Duration
+	mutex    sync.Mutex
+	cache    map[interface{}]entry
+	queMutex sync.Mutex
+	queue    deleteQueue
 }
 
 type entry struct {
@@ -28,6 +31,8 @@ func NewCachedFetcher(
 		ttl:     ttl,
 		cache:   make(map[interface{}]entry),
 	}
+
+	go cached.deleteLoop()
 
 	return cached
 }
@@ -55,18 +60,16 @@ func pickEntry(c *CachedFetcher, key interface{}) entry {
 	var err error
 	done := make(chan struct{})
 	go func() {
-		defer c.deleteKey(key)
-
 		val, err = c.fetcher.Fetch(key)
 		close(done)
 
 		if err != nil {
 			// Don't reuse error values
+			c.queueKey(key, 0)
 			return
 		}
 
-		// Wait for our entry expired
-		time.Sleep(c.ttl)
+		c.queueKey(key, c.ttl)
 	}()
 
 	lazy := func() (interface{}, error) {
@@ -85,4 +88,57 @@ func (c *CachedFetcher) deleteKey(key interface{}) {
 	defer c.mutex.Unlock()
 
 	delete(c.cache, key)
+}
+
+func (c *CachedFetcher) queueKey(key interface{}, ttl time.Duration) {
+	c.queMutex.Lock()
+	defer c.queMutex.Unlock()
+
+	item := deleteItem{key, time.Now().Add(ttl)}
+	heap.Push(&c.queue, item)
+}
+
+func (c *CachedFetcher) deleteLoop() {
+	for {
+		c.queMutex.Lock()
+		if c.queue.Len() > 0 {
+			item := heap.Pop(&c.queue).(deleteItem)
+			if item.expire.Before(time.Now()) {
+				c.deleteKey(item.key)
+			} else {
+				heap.Push(&c.queue, item)
+			}
+		}
+		c.queMutex.Unlock()
+
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+type deleteItem struct {
+	key    interface{}
+	expire time.Time
+}
+
+type deleteQueue []deleteItem
+
+func (dq deleteQueue) Len() int { return len(dq) }
+
+func (dq deleteQueue) Less(i, j int) bool {
+	return dq[i].expire.Before(dq[j].expire)
+}
+
+func (dq deleteQueue) Swap(i, j int) {
+	dq[i], dq[j] = dq[j], dq[i]
+}
+
+func (dq *deleteQueue) Push(x interface{}) {
+	*dq = append(*dq, x.(deleteItem))
+}
+
+func (dq *deleteQueue) Pop() interface{} {
+	n := len(*dq)
+	ret := (*dq)[n-1]
+	*dq = (*dq)[0 : n-1]
+	return ret
 }
